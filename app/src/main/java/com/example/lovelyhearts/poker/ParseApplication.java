4a4ae8b4e6664121a0a8d28af401a53551f05260
aeldred <aeldred@gmail.com>
2015-05-06 18:12:53 -0500

    Parse.com fixes

--- app/src/main/java/com/example/lovelyhearts/poker/ParseApplication.java
+++ app/src/main/java/com/example/lovelyhearts/poker/ParseApplication.java
@@ -7,9 +7,12 @@ package com.example.lovelyhearts.poker;
         import android.app.Application;
 
         import com.parse.Parse;
+        import com.parse.ParseObject;
         import com.parse.ParseACL;
         import com.parse.ParseCrashReporting;
         import com.parse.ParseUser;
+
+
 
 public class ParseApplication extends Application {
 
@@ -22,6 +25,12 @@ public class ParseApplication extends Application {
 
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
+
+        // register subclasses
+        ParseObject.registerSubclass(Location.class);
+        ParseObject.registerSubclass(Player.class);
+        ParseObject.registerSubclass(Seat.class);
+        ParseObject.registerSubclass(Table.class);
 
         // Add your initialization code here
         Parse.initialize(this, "CMGQQp6k1D8jVqr39taB6iEVwLSV39VNJq6SmEJe", "uSZcKaAIdtdQ8xxbiTMmRZTDiXiwT5FnSiOT6bLt");
